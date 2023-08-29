87638d33602e2f15be10a61aaae17602d50039b9
nehemie-tns <nniyomahoro@tns.org>
2023-08-29 11:37:06 +0200

    fix: environment variables

--- analyser/settings/production.py
+++ analyser/settings/production.py
@@ -12,18 +12,18 @@ ENV_ROLE = 'prod'
 if USE_S3 == 'False':
     # we are using the local file system to serve static files, so set the paths appropriately
     # STATIC_ROOT = '/opt/tafiti/static/'
-    STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+    STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname('../'), 'static'))
 
 # Added 
-STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname('../'), 'static'))
 STATIC_URL  = '/static/'
 
 STATICFILES_DIRS = [
     # '/opt/tafiti/analyser/static/'
-    os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+    os.path.abspath(os.path.join(os.path.dirname('../'), 'static'))
 ]
 
-STATIC_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+STATIC_PATH = os.path.abspath(os.path.join(os.path.dirname('../'), 'static'))
 ADMIN_EMAILS = ['nniyomahoro@tns.org', 'smbonyumuhire@tns.org']
 
 # The default port to serve the application from
