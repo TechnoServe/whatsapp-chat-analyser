6783228191dda7b97235e69805b9594aeb1e2f43
nehemie-tns <nniyomahoro@tns.org>
2023-09-07 14:33:49 +0200

    fix: fix

--- analyser/view/tca_views.py
+++ analyser/view/tca_views.py
@@ -1420,7 +1420,8 @@ def process_new_chats(request):
     try:
         params = get_basic_info(request)
         analyser = Analyser()
-        analyser.process_uploaded_files()
+        # TODO (Done) removed this again
+        # analyser.process_uploaded_files()
         analyser.process_pending_chats()
         info_message = "The processing has completed successfully"
 
